d1b17ca38b8bbc3d5878aa72f88f0c4cc83b338c
Shriram <shriram.r@technovert.com>
2022-06-11 22:38:38 +0530

    Add background.js and manifest

---
+++ background.js
@@ -0,0 +1,19 @@
+const changeAllAspectRatios = () => {
+  const changeAspectRatio = (video) => {
+    // get video container div
+    var videoContainer = video.parentNode;
+
+    // add style to div transform: scaleX(0.75); and scaleY(1);
+    videoContainer.style.transform = "scaleX(0.75) scaleY(1)";
+  };
+  document.querySelectorAll("video").forEach(changeAspectRatio);
+};
+
+chrome.action.onClicked.addListener((tab) => {
+  if (!tab.url.includes("chrome://")) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id, allFrames: true },
+      function: changeAllAspectRatios,
+    });
+  }
+});
